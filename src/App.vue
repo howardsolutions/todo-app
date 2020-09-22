06deea63de91879e8f8f1f6de9e452ac84658552
Thinh Phung <thinhphungdev@Thinh-Phungs-Macbook-Pro.local>
2020-09-22 03:58:42 -0700

    push all file to github

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <AddToDo v-on:add-todo = "addToDo" />
+    <Todos v-bind:todos = "todos" v-on:del-todo = "deleteTodo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from "./components/Todos";
+import Header from ".//components/layout/Header";
+import AddToDo from ".//components/AddToDo";
+import axios from 'axios'
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
+    Todos,
+     Header,
+     AddToDo 
+  },
+
+  data() {
+    return {
+      todos: []
+    }
+  },
+
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+
+    addToDo(newTodo) {
+      const { title, completed } = newTodo;
+      axios.post("https://jsonplaceholder.typicode.com/todos", {
+        title,
+        completed 
+      })
+      .then(res =>  this.todos = [...this.todos, res.data])
+      // .catch(err => console.log)
+
+     ;
+    }
+  },
+
+  created() {
+    axios.get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+    .then(response => this.todos = response.data)
+    .catch(err => console.log(err));
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
 </style>
---
+++ src/components/AddToDo.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <form @submit = "addToDo">
+            <input type="text" v-model = "title" name="title" placeholder="add todo">
+            <input type="submit" value="Submit" class="btn">
+        </form>
+    </div>
+</template>
+
+<script>
+// import uuid from 'uuid';
+export default {
+    name: 'AddToDo',
+
+    data() {
+        return {
+            title: ''
+        }
+    },
+    
+    methods: {
+        addToDo(e) {
+            // we dont want the form try to submit to a file
+            e.preventDefault();
+
+            const newTodo = {
+                title: this.title,
+                completed: false
+            } 
+            // Send event (add new to do) to parent
+            this.$emit('add-todo', newTodo)
+        }
+    }
+}
+</script>
+
+<style scoped>
+  form {
+    display: flex;
+  }
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+  input[type="submit"] {
+    flex: 2;
+  }
+</style>
